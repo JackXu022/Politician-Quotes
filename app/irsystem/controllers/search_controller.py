7d24e0eb19420ad1092b49d51463358c604ad623
arzumm <am2692@cornell.edu>
2020-04-19 21:32:09 -0400

    changed output data

--- app/irsystem/controllers/search_controller.py
+++ app/irsystem/controllers/search_controller.py
@@ -16,8 +16,7 @@ def search():
 		output_message = ''
 	else:
 		output_message = "Your search: " + query
-		# data = get_top_n_related(query)
-		data = range(5)
+		data = get_top_n_related(query)
 		# print(data)
 	return render_template('search.html', name=project_name, netid=net_id, output_message=output_message, data=data)
 
