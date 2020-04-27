ac4eef2a698e6ae055a8dfdcf33af208687d24ac
aleahdm <adm265@cornell.edu>
2020-04-26 22:34:18 -0400

    Edited search algorithm

--- app/irsystem/models/search_algorithm.py
+++ app/irsystem/models/search_algorithm.py
@@ -1,31 +1,8 @@
 from operator import itemgetter
 import pandas as pd
-import spacy
 import numpy as np
 from app.irsystem.models.BingImageSearchv7 import image_search
-
-nlp = spacy.load('en_core_web_lg')
-vocab = [w for w in nlp.vocab if w.is_lower and w.prob >= -15 and w.vector.any()]
-vocab_vectors = np.array([w.vector for w in vocab])
-
-def most_similar_by_embedding(word_vector, top=4):
-    sim = np.inner(word_vector, vocab_vectors)
-    by_similarity = [vocab[i] for i in np.argsort(-sim)]
-    top_similar = by_similarity[:top]
-    return [w.lower_ for w in top_similar]
-
-def most_similar(word: str, top=4):
-    words = word.split(" ")
-    output = []
-    for word in words:
-        word = word.strip()
-        word_vector = nlp.vocab[word].vector
-        if not np.any(word_vector):
-            return [word]
-        most_sim = most_similar_by_embedding(word_vector, top)
-        most_sim.remove(word)
-        output += most_sim
-    return output
+from .thesaurus import Thesaurus
 
 def get_top_n_related(topic, n, politicians={}):
     if n:
@@ -54,7 +31,7 @@ def get_top_n_related(topic, n, politicians={}):
         topics = []
         topics += input
         for topic in input:
-            syns = most_similar(topic)
+            syns = Thesaurus.most_similar(topic)
             topics += syns
         wc_matrix = np.zeros((len(topics), len(debate_data.index)))
         score_matrix = np.zeros((len(debate_data.index,)))
