37966d0147ac08c1b01025263f47849942e0c591
arzumm <am2692@cornell.edu>
2020-04-26 23:48:49 -0400

    added route and files for version2

--- app/irsystem/controllers/search_controller.py
+++ app/irsystem/controllers/search_controller.py
@@ -3,7 +3,7 @@ from app.irsystem.models.helpers import *
 from app.irsystem.models.helpers import NumpyEncoder as NumpyEncoder
 
 
-from app.irsystem.models.search_algorithm import get_top_n_related
+from app.irsystem.models.search_algorithm_v1 import get_top_n_related
 
 project_name = "Politician Quotes"
 net_id = "Arzu Mammadova: am2692, Aleah Markovic: adm265, Matthew Price: mp836, Zhaopeng Xu: zx273"
@@ -23,3 +23,15 @@ def search():
 
 
 
+@irsystem.route('/v2', methods=['GET'])
+def search_2():
+	topic = request.args.get('topic')
+	politicians = request.args.get('politicians')
+	if not topic and not politicians:
+		data = []
+		output_message = ''
+	else:
+		output_message = "Topics: " + topic + '\n' + "Politicians: " + politicians
+		data = get_top_n_related(topic, 10, politicians)
+		# print(data)
+	return render_template('search-v2.html', name=project_name, netid=net_id, output_message=output_message, data=data)
