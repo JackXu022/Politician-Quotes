9054d83f7a07869d2f978f18d833218ddadd4695
arzumm <am2692@cornell.edu>
2020-05-02 19:48:02 -0400

    created files and routes for the final version, implemented highlight functionality

--- app/irsystem/controllers/search_controller.py
+++ app/irsystem/controllers/search_controller.py
@@ -10,6 +10,8 @@ from app.irsystem.models.search_algorithm_v2 import get_top_n_related_v2
 project_name = "Politician Quotes"
 net_id = "Arzu Mammadova: am2692, Aleah Markovic: adm265, Matthew Price: mp836, Zhaopeng Xu: zx273"
 
+      
+
 @irsystem.route('/', methods=['GET'])
 def search():
 	topic = request.args.get('topic')
@@ -18,10 +20,13 @@ def search():
 		data = []
 		output_message = ''
 	else:
-		output_message = "Topics: " + topic + '\n' + "Politicians: " + politicians
-		data = get_top_n_related(topic, 10, politicians)
-		# print(data)
-	return render_template('search.html', name=project_name, netid=net_id, output_message=output_message, data=data)
+		data = get_top_n_related_v2(topic, 10, politicians)
+		if data:
+			output_message = "Topics: " + topic + '\n' + "Politicians: " + politicians
+		else: 
+			output_message = 'No results found'
+		
+	return render_template('search-final.html', name=project_name, netid=net_id, output_message=output_message, topic=topic, data=data)
 
 
 
@@ -33,7 +38,22 @@ def search_2():
 		data = []
 		output_message = ''
 	else:
-		output_message = "Topics: " + topic + '\n' + "Politicians: " + politicians
 		data = get_top_n_related_v2(topic, 10, politicians)
-		# print(data)
+		output_message = "Topics: " + topic + '\n' + "Politicians: " + politicians
+			
 	return render_template('search-v2.html', name=project_name, netid=net_id, output_message=output_message, data=data)
+
+
+
+@irsystem.route('/v1', methods = ['GET'])
+def search_1(): 
+	topic = request.args.get('topic')
+	politicians = request.args.get('politicians')
+	if not topic and not politicians:
+		data = []
+		output_message = ''
+	else:
+		data = get_top_n_related(topic, 10, politicians)
+		output_message = "Topics: " + topic + '\n' + "Politicians: " + politicians
+		
+	return render_template('search.html', name=project_name, netid=net_id, output_message=output_message, data=data)
