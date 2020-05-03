d0993c0e49d3bfc8d3c792505732ffef934e5952
arzumm <am2692@cornell.edu>
2020-05-03 19:21:37 -0400

    changed the names function, added year input

--- app/irsystem/controllers/search_controller.py
+++ app/irsystem/controllers/search_controller.py
@@ -13,18 +13,14 @@ from app.data.name_data import names
 project_name = "Politician Quotes"
 net_id = "Arzu Mammadova: am2692, Aleah Markovic: adm265, Matthew Price: mp836, Zhaopeng Xu: zx273"
 
-name_list = set()
-for item in dict(names).items():
-	name = item[1][0]
-	name_list.add(name)
-name_list = list(name_list)
-name_list = json.dumps(name_list)
+
 
 @irsystem.route('/', methods=['GET'])
 def search():
 	topic = request.args.get('topic')
 	politicians = request.args.get('politicians')
 	party = request.args.get('party')
+	year = request.args.get('year')
 	if not topic and not politicians:
 		data = []
 		output_message = ''
@@ -39,6 +35,12 @@ def search():
 
 @irsystem.route('/names', methods = ['GET'])
 def names(): 
+	name_list = set()
+	for item in dict(names).items():
+		name = item[1][0]
+		name_list.add(name)
+	name_list = list(name_list)
+	name_list = json.dumps(name_list)
 	return name_list
 
 @irsystem.route('/v2', methods=['GET'])
